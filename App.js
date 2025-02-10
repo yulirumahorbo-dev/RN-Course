9e9d5f591e175279328a6b97d1694f3d8c6e4b12
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-10 11:06:51 +0700

    style: setting background color for all screen, set light color for status bar

--- App.js
+++ App.js
@@ -31,43 +31,51 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color={"#5e0acc"}
-        onPress={startAddGoalHandler}
-      />
-
-      <GoalInput
-        visible={modalIsVisible}
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-      />
-
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler}
-              />
-            );
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color={"#5e0acc"}
+          onPress={startAddGoalHandler}
         />
+
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: { flex: 1, padding: 50, paddingTop: 50, paddingHorizontal: 16 },
+  appContainer: {
+    flex: 1,
+    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
 
   goalsContainer: { flex: 5 },
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -36,10 +36,14 @@ function GoalInput(props) {
 
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Cancel" onPress={props.onCancel} color={"#f31282"} />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button
+              title="Add Goal"
+              onPress={addGoalHandler}
+              color={"#5e0acc"}
+            />
           </View>
         </View>
       </View>
@@ -59,9 +63,12 @@ const styles = StyleSheet.create({
   image: { width: 100, height: 100, margin: 20 },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    borderRadius: 6,
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
     width: "100%",
-    padding: 8,
+    padding: 16,
   },
   buttonContainer: {
     flexDirection: "row",
