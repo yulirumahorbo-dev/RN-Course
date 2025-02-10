016605bd63ee10e6099467df984e5268eac454a4
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-10 10:54:23 +0700

    style: adding image & changing background color the modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -16,6 +23,10 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal"
@@ -43,10 +54,9 @@ const styles = StyleSheet.create({
     padding: 16,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
-    borderWidth: 1,
-    borderColor: "#cccccc",
+    backgroundColor: "#311b6b",
   },
+  image: { width: 100, height: 100, margin: 20 },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
